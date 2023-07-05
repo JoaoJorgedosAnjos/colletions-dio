1b9ca56a9e1c436ddcd84745f110975bc27bedf0
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-05 15:59:35 -0300

    Conhecendo os métodos List - ex 01

--- src/com/dio/collection/list/ExExemploList.java
+++ src/com/dio/collection/list/ExExemploList.java
@@ -3,6 +3,8 @@ package com.dio.collection.list;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class ExExemploList {
     public static void main(String[] args) {
         List<Double> notas = new ArrayList<>();
@@ -18,6 +20,16 @@ public class ExExemploList {
         System.out.println(notas);
 
     }
+    /*
+Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
+
+        System.out.println("Crie uma lista chamada notas2 " +
+                "e coloque todos os elementos da list Arraylist nessa nova lista: ");
+
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: ");
+
+        System.out.println("Mostre a primeira nota da nova lista removendo-o: ");
+*/
 
 
 }
